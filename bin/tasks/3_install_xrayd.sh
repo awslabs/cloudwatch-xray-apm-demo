982e6380ac49f3d4213c353df9b48aa5c3eb2951
Martin Schade <schadix@gmail.com>
2017-11-28 12:13:12 -0800

    works with other regions, moved install tasks to CloudFormation
    * works with other regions beyond us-east-1
    * moved install tasks to CloudFormation, deleted the old task files

--- bin/tasks/3_install_xrayd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~
-curl https://s3.dualstack.us-east-2.amazonaws.com/aws-xray-assets.us-east-2/xray-daemon/aws-xray-daemon-2.x.rpm -o /home/ec2-user/xray.rpm
-sudo yum install -y /home/ec2-user/xray.rpm
